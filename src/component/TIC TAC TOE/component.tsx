0e90d2a0cbe053dd912578d3598c4195ef6b7c23
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-04 19:55:44 +0100

    handling functionality

--- src/component/TIC TAC TOE/component.tsx
+++ src/component/TIC TAC TOE/component.tsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import '../style/style.css'
 
-const Component = () => {
+type ComponentProps = {
+    setBox : (e: React.MouseEvent<HTMLDivElement>, num: number) => void,
+    changeTitle: React.MutableRefObject<HTMLDivElement>
+    reset: () => void
+    resetTitle: React.MutableRefObject<HTMLDivElement>
+}
+
+const Component = ({setBox, changeTitle, reset, resetTitle}:ComponentProps) => {
   return (
     <div className='container'>
-        <h1 className="title">TIC TAC TOE <span>REACT</span></h1>
+        <h1 className="title" ref={changeTitle}>TIC TAC TOE <span>REACT</span></h1>
       <div className='box-wrapper'>
         <div className='row'>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 1)}}></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 2)}}></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 3)}}></div>
         </div>
         <div className='row'>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 4)}}></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 5)}}></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 6)}}></div>
         </div>
         <div className='row'>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 7)}}></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 8)}}></div>
+            <div className='box' ref={resetTitle} onClick={(e)=>{setBox(e, 9)}}></div>
         </div>
       </div>
-      <button className='reset'>RESET</button>
+      <button className='reset' onClick={reset}>RESET</button>
     </div>
   )
 }
--- src/component/TIC TAC TOE/tictactoe.tsx
+++ src/component/TIC TAC TOE/tictactoe.tsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Component from './component'
+import cross_img from '../assets/cross.png'
+import circle_img from '../assets/circle.png'
 
+var arr = ["","","","","","","","",""]
 const TicTacToe = () => {
+    const [count, setCount] = useState(0)
+    const [lock, setLock] = useState(false)
+    const changeTextRef = useRef<HTMLDivElement>(null!)
+    const resetRef = useRef<HTMLDivElement>(null!)
+
+    const setBox = (e:React.MouseEvent<HTMLDivElement>, num:number)=>{
+        if(lock){
+           return; 
+        }
+        else{
+            if(count%2==0){
+                (e.target as HTMLDivElement).innerHTML  = `<img src=${cross_img}>`
+                setCount(prevCount=>prevCount+1)
+                arr[num] = "x"
+                checkWin(cross_img)
+            }
+            else{
+                (e.target as HTMLDivElement).innerHTML  = `<img src=${circle_img}>`
+                setCount(prevCount=>prevCount+1)
+                arr[num] = "o"
+                checkWin(circle_img) 
+            }
+        }
+    }
+
+    const checkWin = (text:string)=>{
+        for(let i=0; i<arr.length; i+=3){
+            if(arr[i]===arr[i+1]&&arr[i+1]===arr[i+2]&&arr[i+2]!==""){
+                winner(text)
+            }
+        }
+        for(let i=0; i<arr.length; i++){
+            if(arr[i]===arr[i+3]&&arr[i+3]===arr[i+6]&&arr[i+6]!==""){
+                winner(text)
+            }
+        }
+        for(let i=0; i<arr.length; i+=2){
+            if(arr[i]===arr[i+4]&&arr[i+4]===arr[i+8]&&arr[i+8]!==""){
+                winner(text)
+            }
+        }         
+    }
+    const winner = (text:string)=>{
+        changeTextRef.current.innerHTML = `CONGRATULATIONS <img src=${text}> WINS`
+        setLock(true)
+    }
+
+    const reset = ()=>{
+        resetRef.current.innerHTML = ""
+        arr = ["","","","","","","","",""]
+        setLock(false)
+        setCount(0)
+    }
+
   return (
     <div>
-      <Component/>
+      <Component setBox = {setBox} changeTitle={changeTextRef} reset={reset} resetTitle={resetRef}/>
     </div>
   )
 }
